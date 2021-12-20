2eebf3b455161a9b3151f849674bfb6b1ac012ee
Donald Wu <yeukfei02@gmail.com>
2021-12-20 21:00:14 +0800

    feat: 🎸 add code test

--- test/testSuite.test.ts
+++ test/testSuite.test.ts
@@ -1,7 +1,9 @@
 import { userTest } from "./user";
 import { companyTest } from "./company";
+import { codeTest } from "./code";
 
 describe("multivendor api test", () => {
   userTest();
   companyTest();
+  codeTest();
 });
